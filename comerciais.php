bffb5b1697d112cec57c22c197b550309eebbbce
marcelo vinicius <74020188+marceloVinicius12@users.noreply.github.com>
2021-07-07 22:48:08 -0300

    Add files via upload

---
+++ comerciais.php
@@ -0,0 +1,31 @@
+<?php include "includes/cabecalho.php"
+?>
+      <div class="row">
+        <div class="text-center mt-5">
+      <h1>projetos comerciais</h1>
+ <hr>
+        </div>
+      </div>
+      <div class="row">
+        <div class="text-center">
+      <img src="img/restaurante-desenho.jpg" class="img-fluid" alt="restaurante-desenho">
+      <div class="text-center">
+        <h4 class="mt-0">a segunda casa </h4>
+        <p>acreditamos que o estabelecimento ou area de trabalho é nossa segunda casa por isso buscamos trazer o maximo de conforto </p>
+        <p class="mb-0"> e comodidade aos  funcionario e ao cliente </p>
+      <hr>
+      </div>
+    </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col text-center">
+          <a href="comerciais/plaza.php"> <img src="img/fachada-restaurante.jpg " class="img-fluid img-thumbnail" style="width: 200rem;">
+            <a href="comerciais/plaza.php" class="text-decoration-none text-dark" alt="imagem-restaurante-plaza"><h4>Restaurante Plaza</h4></a>
+        </div>
+        <div class="col text-center">
+          <a href="comerciais/hm.php"><img src="img/resta.jpg" class="img-fluid img-thumbnail" style="width: 200rem;">
+            <a href="comerciais/hm.php" class="text-decoration-none text-dark" alt="restaurante-hm"><h4>Restaurante HM</h4></a>
+        </div>
+      </div>
+      <?php include "includes/rodape.php"
+      ?>
