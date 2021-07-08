9f7ae5e84cd3b26e6ac9bce0b3eb70e61e60c686
marcelo vinicius <74020188+marceloVinicius12@users.noreply.github.com>
2021-07-07 22:46:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+include "includes/cabecalho.php"
+?>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item  active">
+            <li><a href="residencias/vale-verde.php"><img src="img/vale-verde.jpg" class="d-block w-100" alt="residencial-vale-verde">
+            <div class="carousel-caption d-none d-md-block">
+              <h2>Residencial Vale-Verde</h2>
+              <p>projeto residencial</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <li><a href="residencias/campo-belo.php"><img src="img/limeira-house-f.jpg" class="d-block w-100" alt="residencial-campo-belo">
+            <div class="carousel-caption d-none d-md-block ">
+              <h2>Residencial Campo Belo</h2>
+              <p>projeto residencial </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <li><a href="residencias/alphaville.php"> <img src="img/alphaville.jpg" class="d-block w-100" alt="residencial-alphaville">
+            <div class="carousel-caption d-none d-md-block">
+              <h2>Residencial Alphaville</h2>
+              <p>projeto residencial</p>
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <li><a href="comerciais/hm.php"><img src="img/resta.jpg" class="d-block w-100" alt="restaurante-hm">
+            <div class="carousel-caption d-none d-md-block">
+              <h2>Restaurante HM </h2>
+              <p>projeto comercial </p>
+      </div>
+</a>
+          </div>
+        </div>
+</head>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+      <div class="container">
+      <div class="row mt-5  text-center ">
+        <div class="col-lg-4 col-md-6 text-center">
+      <div class="card" style="width: 20rem; ">
+        <img class="card-img-top" src="img/casa-primavera.jpg" alt="residencial-jardim-paulista">
+        <div class="card-body">
+          <h2 class="card-title text-center">residenciais</h2>
+          <p class="card-text">Veja nossos projetos de <strong>casas</strong> e <strong>interiores</strong> .Tornando real o desejo daqueles que o procuram para construir e morar na casa que sempre sonharam</p>
+          <a href="residencias.php" class="btn btn-primary">ver projetos</a>
+        </div>
+      </div>
+        </div>
+      <div class="col-lg-4 col-md-6 text-center">
+      <div class="card" style="width: 20rem;">
+        <img class="card-img-top" src="img/fachada-restaurante.jpg" alt="imagem-restaurante-plaza">
+        <div class="card-body">
+          <h2 class="card-title ">comerciais </h2>
+          <p class="card-text">veja os nossos projetos <strong>comerciais</strong> cuidadosamente criados para melhor experiencia dos funcionarios e aos clientes que visitam  o local</p>
+          <a href="comerciais.php" class="btn btn-primary">ver projetos</a>
+        </div>
+      </div>
+      </div>
+
+      <div class="col-lg-4  col-md-3 text-center ">
+        
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="img/teste.jpg" alt="quem-somos-imagens">
+          <div class="card-body">
+            <h2 class="card-title ">quem somos </h2>
+            <p class="card-text">conheça mais sobre nossa empresa , nossa historia, valores e principais ideias <br><br><br></p>
+            <a href="quemsomos.php" class="btn btn-primary">conheça mais </a>
+          </div>
+      
+        </div>
+      </div>
+      </div>
+      </div>
+      <div class="card mt-5 text-center">
+        <div class="card-header">
+          <h3>Arquitetura</h3>
+        </div>
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>"De um traço nasce a arquitetura. E quando ele é bonito e cria surpresa,<br> ela pode atingir, sendo bem conduzida,<br> o nível superior de uma obra de arte."</p>
+            <div class="blockquote-footer"><cite title="Título da fonte">Oscar Niemeyer</cite></div>
+          </blockquote>
+        </div>
+      </div>
+      <?php include "includes/rodape.php"
+      ?>
---
+++ quemsomos.php
@@ -0,0 +1,55 @@
+<?php include "includes/cabecalho.php"
+?>
+      <div class="row text-center mt-2">
+      <div class="col-md-12 text-center ">
+        <h1>quem somos </h1>
+   <hr>
+      </div>
+      </div>
+      <div class="row mt-3 text-center">
+        <div class="col-sm-6 text-left ">
+                <img src="img/quem-somos.jpg" class="img-fluid" >
+          </div>
+          <div class="col-sm-6 text-right fs-5">
+            <h2>ideais e valores </h2>
+            <p>nossa empresa  prioriza trabalhar em parceria com os nossos clientes, visando um bom planejamento e técnicas metodológicas de trabalho,<br> sendo possível a redução do tempo de execução, custo e principalmente fazer com que conquistem seus objetivos com satisfação e êxito.</p>
+            <i class="bi bi-arrow-down-square-fill text-primary"></i>
+          </div>
+      </div>
+      <hr>
+      <div class="row mt-3 text-center">
+        <div class="col-sm-6 text-left fs-5">
+                <h2>analise</h2>
+                <p>todos os projetos sao revisadas e analisadas por nossa equipe  visando sempre criar o ambiente perfeito ao cliente e com o melhor custo beneficio possivel e utilizando nossa propria indentidade  </p>
+                
+            </div>
+            <div class="col-sm-6 text-right fs-5">
+        <img src="img/analise.png"  class="img-fluid "  >
+        
+        </div>
+        <i class="bi bi-arrow-down-square-fill text-primary"></i>
+      </div>
+      <hr>
+      <div class="row mt-3 text-center">
+        <div class="col-sm-6 text-left ">
+                <img src="img/aprovacao.png" class="img-fluid" >
+          </div>
+          <div class="col-sm-6 text-right fs-5 mt-2">
+            <h2>aprovações </h2>
+            <p>Assim que concretizarmos o projeto arquitetônico, iniciamos o desenvolvimento dele simplificado para aprovação na prefeitura. Como funciona? Ficaremos à frente de toda a tramitação, desde o desenvolvimento até a emissão dos protocolos. Finalizado este processo, lhe entregaremos a aprovação para iniciar a construção.</p>
+            <i class="bi bi-arrow-down-square-fill text-primary"></i>
+        </div>
+      </div>
+      <hr>
+      <div class="row mt-3 text-center">
+        <div class="col-sm-6 text-left fs-5">
+                <h2>construção dos seus sonhos </h2>
+                <p>apos analisarmos a area e o projeto ser aprovado iniciamos a construção com a junção de nossos profissionais altamente capacidados e o que foi passado de ideias pelo nossos clientes , sempre com o melhor custo beneficio possivel e em um curto de tempo </p>
+             
+            </div>
+            <div class="col-sm-6 text-right fs-5">
+        <img src="img/criacao.png"  class="img-fluid" >
+        </div>
+      </div>
+      <?php include "includes/rodape.php"
+      ?>
